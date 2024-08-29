9aa1e061c796b307f65046448ed6863df52156fe
Dewangga Dika Darmawan <dewangga3d@gmail.com>
2024-08-29 07:27:39 +0700

    fix: http header response

--- app/middlewares/authMiddleware.js
+++ app/middlewares/authMiddleware.js
@@ -1,11 +1,15 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 jwtSecret = process.env.JWT_SECRET;
 
 module.exports = (req, res, next) => {
-  const token = req.cookies.token;
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(" ")[1];
 
   if (!token) {
-    return res.status(401).json({ status: 'error', message: 'You must be logged in to access this resource' });
+    return res.status(401).json({
+      status: "error",
+      message: "You must be logged in to access this resource",
+    });
   }
 
   try {
@@ -13,6 +17,6 @@ module.exports = (req, res, next) => {
     req.userId = decoded.userId;
     next();
   } catch (err) {
-    return res.status(401).json({ status: 'error', message: 'Invalid token' });
+    return res.status(401).json({ status: "error", message: "Invalid token" });
   }
 };
